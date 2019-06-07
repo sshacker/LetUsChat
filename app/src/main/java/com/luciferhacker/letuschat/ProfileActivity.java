82ac88e025d7aa79fcf27d4539bc5d25a2ba970b
sshacker <shivamsonkar260@gmail.com>
2019-06-07 16:54:00 +0530

    17 friends request sending

--- app/src/main/java/com/luciferhacker/letuschat/ProfileActivity.java
+++ app/src/main/java/com/luciferhacker/letuschat/ProfileActivity.java
@@ -1,26 +1,111 @@
 package com.luciferhacker.letuschat;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView mProfileId;
+    private ImageView mProfileImage;
+    private TextView mProfileName, mProfileStatus, mProfileFriendsCount;
+    private Button mProfileSendReqBtn;
+
+    private DatabaseReference mUsersDatabase;
+    private DatabaseReference mFriendReqDatabase;
+    private FirebaseUser mCurrent_user;
+    private ProgressDialog mProgressDialog;
+    private String mCurrent_state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        String user_id = getIntent().getStringExtra("user_id");
-        mProfileId = (TextView)findViewById(R.id.profile_displayName);
-        mProfileId.setText(user_id);
+        mProfileImage = (ImageView)findViewById(R.id.profile_image);
+        mProfileName = (TextView)findViewById(R.id.profile_displayName);
+        mProfileStatus = (TextView)findViewById(R.id.profile_status);
+        mProfileFriendsCount = (TextView)findViewById(R.id.profile_totalFriends);
+        mProfileSendReqBtn = (Button)findViewById(R.id.profile_send_req_btn);
 
+        mCurrent_state = "not_friends";
+
+        final String user_id = getIntent().getStringExtra("user_id");
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("User").child(user_id);
+        mFriendReqDatabase = FirebaseDatabase.getInstance().getReference().child("Friend_req");
+        mCurrent_user = FirebaseAuth.getInstance().getCurrentUser();
+
+        mProgressDialog = new ProgressDialog(ProfileActivity.this);
+        mProgressDialog.setTitle("Loading User Profile");
+        mProgressDialog.setMessage("Please wait !");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+
+        mUsersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String display_name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                mProfileName.setText(display_name);
+                mProfileStatus.setText(status);
+
+                Picasso.get().load(image).placeholder(R.drawable.default_avatar).into(mProfileImage);
+                mProgressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mProfileSendReqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrent_state.equals("not_friends")){
+
+                    mFriendReqDatabase.child(mCurrent_user.getUid()).child(user_id).child("request_type")
+                            .setValue("sent").addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+                                mFriendReqDatabase.child(user_id).child(mCurrent_user.getUid()).child("request_type")
+                                        .setValue("request").addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(ProfileActivity.this, "Request Send",Toast.LENGTH_LONG).show();
+                                    }
+                                });
+
+                            } else {
+                                Toast.makeText(ProfileActivity.this, "Failed sending Request",Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
     }
 }
