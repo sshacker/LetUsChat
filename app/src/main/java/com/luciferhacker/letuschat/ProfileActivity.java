2772948c3bbdee0d7362ee2aecd2ee0926e5bcb4
sshacker <shivamsonkar260@gmail.com>
2019-07-01 19:07:17 +0530

    21 adding notification feature using cloud massaging of firebase

--- app/src/main/java/com/luciferhacker/letuschat/ProfileActivity.java
+++ app/src/main/java/com/luciferhacker/letuschat/ProfileActivity.java
@@ -1,10 +1,9 @@
 package com.luciferhacker.letuschat;
 
 import android.app.ProgressDialog;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
--- app/src/main/java/com/luciferhacker/letuschat/RegisterActivity.java
+++ app/src/main/java/com/luciferhacker/letuschat/RegisterActivity.java
@@ -2,17 +2,16 @@ package com.luciferhacker.letuschat;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.textfield.TextInputLayout;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 //import android.widget.Toolbar;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
